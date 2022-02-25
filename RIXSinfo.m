a5e02f7d82ce8d9e0193cc5a4a27aba5cba0f9b9
Yuan Wei <yuan.wei@psi.ch>
2022-02-25 21:41:00 +0100

    02/25/2022

---
+++ RIXSinfo.m
@@ -0,0 +1,46 @@
+function fileinfo = RIXSinfo(filelist)
+    global filepath;
+    fileinfo = 'info';
+    
+    if iscell(filelist)
+        filename=[filepath,'\',filelist{1}];
+        ee=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/PhotonEnergy')));
+        p=mean(h5read(filename,'/entry/instrument/NDAttributes/PolarMode'));
+        if p == 0
+            pp = "LH";
+        elseif p == 1
+            pp = "LV";
+        elseif p == 2
+            pp = "C+";
+        else
+            pp = "C-";
+        end
+        tt=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/SampleTemp')));
+        xx=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/SampleXs')));
+        yy=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/SampleYs')));
+        zz=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/SampleZ')));
+        thth=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/SampleTheta')));
+        phph=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/SamplePhi')));
+        tltl=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/SampleTilt')));
+        atat=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/AcquireTime')));
+        spsp=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/ExposureSplit')));
+        slsl=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/ExitSlit')));
+        bcbc=sprintf('%.3f',mean(h5read(filename,'/entry/instrument/NDAttributes/BeamCurrent')));
+
+        info = strcat("Energy : ",ee);
+        info = info+newline+strcat("Polarization : ",pp);
+        info = info+newline+strcat("Temperature : ",tt);
+        info = info+newline+strcat("Sample x : ",xx);
+        info = info+newline+strcat("Sample y : ",yy);
+        info = info+newline+strcat("Sample z : ",zz);
+        info = info+newline+strcat("Sample Theta : ",thth);
+        info = info+newline+strcat("Sample Phi : ",phph);
+        info = info+newline+strcat("Sample Tilt : ",tltl);
+        info = info+newline+strcat("Acquire Time : ",atat);
+        info = info+newline+strcat("Exposure Split : ",spsp);
+        info = info+newline+strcat("Exit Slit : ",slsl);
+        info = info+newline+strcat("Beam Current : ",bcbc);
+        
+        fileinfo = info;
+    end
+end
---
+++ RIXSplot.m
@@ -0,0 +1,45 @@
+function [xdata,ydata] = RIXSplot(filelist)
+    global filepath;
+    
+    if iscell(filelist)
+        filenumber=size(filelist,2);
+        for i=1:filenumber
+            filename=[filepath,'\',filelist{i}];
+            if i == 1
+                refdata=h5read(filename,'/entry/analysis/spectrum');
+                pixeldata=[1:size(refdata)];
+                sumdata=refdata;
+            else
+                uncorrdata=h5read(filename,'/entry/analysis/spectrum');
+                corrdata=correlatedata(refdata,uncorrdata);
+                sumdata=sumdata+corrdata;
+            end
+        end
+        avedata=sumdata/filenumber;
+        ydata=avedata;
+        xdata = zeroenergy(pixeldata,avedata);
+    end
+
+end
+
+function corrdata = correlatedata(refdata,uncorrdata)
+    [r,lags] = xcorr(refdata,uncorrdata);
+    [m,p] = max(r);
+    corrdata = circshift(uncorrdata,lags(p));
+end
+
+function energydata = zeroenergy(pixeldata,tempdata)
+    global energydispersion;
+    [pks,locs,w,p] = findpeaks(tempdata,'MinPeakHeight',5,'MinPeakProminence',1,'MinPeakWidth',4);
+    zeropixel = locs(size(locs,1));
+    try
+        zerofit = fitelastic(tempdata,pks,locs,w,p);
+        energydata = (pixeldata - zerofit) *energydispersion;
+    catch
+        energydata = (pixeldata - zeropixel) *energydispersion;
+    end
+end
+
+function zerofit = fitelastic(tempdata,pks,locs,w,p)
+    
+end
